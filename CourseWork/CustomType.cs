f47643c6b3c3059ed656c5fc128b5b1b92af17a2
Cheesh <90328236+Cheesh@users.noreply.github.com>
2023-06-07 20:03:55 +0300

    commit

---
+++ CourseWork/CustomType.cs
@@ -0,0 +1,43 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace CourseWork
+    {
+       public class CustomType
+        {
+            private List<long> data;
+
+            public CustomType()
+            {
+                data = new List<long>();
+            }
+
+            public CustomType(List<long> list)
+            {
+                data = new List<long>(list);
+            }
+
+            public void Add(long item)
+            {
+                data.Add(item);
+            }
+
+            public void SortDescending()
+            {
+                data.Sort((a, b) => b.CompareTo(a));
+            }
+
+            public override string ToString()
+            {
+                string result = "";
+                foreach (long item in data)
+                {
+                    result += item.ToString() + " ";
+                }
+                return result.TrimEnd();
+            }
+        }
+    }
